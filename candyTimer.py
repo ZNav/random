d3d25174d216f611bf3cc92ceb3bc9da3fbb9c6a
Zander Navratil <50341426+ZNav@users.noreply.github.com>
2022-01-30 23:11:30 -0500

    New Toggle Button
    New Toggle Button for optional no windows notifications.

--- candyTimer.py
+++ candyTimer.py
@@ -1,6 +1,7 @@
 #takes time you want candy to last and gives you a notification when the ideal time to eat it is
 
 #imports
+from ast import Not
 import time
 from win10toast import ToastNotifier
 from datetime import datetime
@@ -19,8 +20,10 @@ def calculateTime(timeToLast):
     return timeToLast
 
 
-def runMain(timeToLast, pieces):
+def runMain(timeToLast, pieces, nopopup = False):
     interval = float(timeToLast)/int(pieces)
+    if(nopopup == True):
+        print('-----------------\nYou have selected the no windows pop up option.')
     if(interval < 10):
         dur = interval-1
         interval = 1
@@ -39,34 +42,49 @@ def runMain(timeToLast, pieces):
         pieces = int(pieces) - 1
         if(pieces != 0):
             print('enjoy a piece of candy at '+current_time+', there are '+str(pieces)+' remaining pieces.')
-            ToastNotifier().show_toast('Candy Time!', 'Enjoy a piece of candy! There are '+str(pieces)+' remaining pieces.', duration = dur)
+            if(nopopup == False):
+                ToastNotifier().show_toast('Candy Time!', 'Enjoy a piece of candy! There are '+str(pieces)+' remaining pieces.', duration = dur)
         else:
             print('enjoy the final piece of candy at '+current_time+', there are no remaining pieces.')
-            ToastNotifier().show_toast('Candy Time!', 'Enjoy the final piece of candy! There are no remaining pieces.', duration = 10)
+            if(nopopup == False):
+                ToastNotifier().show_toast('Candy Time!', 'Enjoy the final piece of candy! There are no remaining pieces.', duration = 10)
 
 
 
 #main
 master = Tk()
 master.title('Candy Counter')
+c1Var = False
 def guiGet():
+    global c1Var
     time = e1.get()
     pieces = e2.get()
-    master.destroy()
-    runMain(str(calculateTime(time)),str(pieces))
+    if(c1Var == False):
+        master.destroy()
+    runMain(str(calculateTime(time)),str(pieces), c1Var)
 def guiGetEnter(e):
+    global c1Var
     time = e1.get()
     pieces = e2.get()
-    master.destroy()
-    runMain(str(calculateTime(time)),str(pieces))
-Label(master, text='Time to last:\n45s = 45 seconds, 20m or 20 = 20 minutes 1.5h = 1:30 minutes:').grid(row=0)
-Label(master, text='Amount of Candy:').grid(row=1)
+    if(c1Var == False):
+        master.destroy()
+    runMain(str(calculateTime(time)),str(pieces), c1Var)
+def toggleVal():
+    global c1Var
+    c1Var = not c1Var
+Label(master, text='Time to last:\n45s = 45 seconds, 20m or 20 = 20 minutes 1.5h = 1:30 minutes:').grid(row=0, columnspan=2)
+Label(master, text='Amount of Candy:').grid(row=1, columnspan=2)
+Label(master, text='Disable Windows Pop-Ups?').grid(row=2, column=0)
 e1 = Entry(master)
 e2 = Entry(master)
 b1 = Button(master, text='Enter', command = guiGet)
-e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
-b1.grid(row=2, column=0)
+b2 = Button(master, text='Exit', command = master.destroy)
+c1 = Checkbutton(master, command= toggleVal)
+e1.grid(row=0, column=2, columnspan=2)
+e2.grid(row=1, column=2, columnspan=2)
+c1.grid(row=2, column=1)
+b1.grid(row=2, column=2)
+b2.grid(row=2, column=3)
 master.bind('<Return>',guiGetEnter)
 e1.focus()
 master.mainloop()
